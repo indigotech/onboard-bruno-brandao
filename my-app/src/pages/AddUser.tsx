b45e6a4eb9eb989da3ee88b329d87cf3b2af76f5
Bruno Brandão <bruno.brandao@taqtile.com.br>
2024-05-20 16:13:23 -0300

    fix tailwind

--- my-app/src/pages/AddUser.tsx
+++ my-app/src/pages/AddUser.tsx
@@ -1,5 +1,4 @@
 import { FormEvent, useState } from "react";
-import "../styles/Adduser.css";
 import taq from "../img/Logo_Quadrado_Preto.png";
 import {
   validateBirthDate,
@@ -18,111 +17,212 @@ function AddUser() {
   const [phone, setPhone] = useState("");
   const [role, setRole] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [birthDateError, setBirthDateError] = useState("");
+  const [roleError, setRoleError] = useState("");
   const [createUser, { loading }] = useMutation(CREATE_USER);
   const navigate = useNavigate();
 
   async function handleSignup(e: FormEvent) {
     e.preventDefault();
+    let hasError = false;
 
-    switch (true) {
-      case email === "":
-        return alert("O campo de email não pode estar vazio.");
-      case !validateEmail.test(email):
-        return alert("Por favor digite um e-mail válido! ");
-      case name === "":
-        return alert("O campo de nome não pode estar vazio.");
-      case password === "":
-        return alert("O campo de senha não pode estar vazio.");
-      case !validatePassword.test(password):
-        return alert(
-          "A senha deve ter no mínimo 7 caracteres sendo ao menos uma letra e um dígito",
-        );
-      case phone === "":
-        return alert("O campo de telefone não pode estar vazio.");
-      case role === "":
-        return alert("O campo de role não pode estar vazio.");
-      case birthDate === "":
-        return alert("O campo de data de nascimento não pode estar vazio.");
-      case !validateBirthDate(birthDate):
-        return alert(
-          "A data de nascimento é inválida ou você é menor de 18 anos.",
-        );
-      default:
+    if (email === "") {
+      setEmailError("O campo de email não pode estar vazio.");
+      hasError = true;
+    } else if (!validateEmail.test(email)) {
+      setEmailError("Por favor, insira um e-mail válido.");
+      hasError = true;
+    } else {
+      setEmailError("");
     }
 
-    try {
-      await createUser({
-        variables: {
-          input: { name, email, phone, role, birthDate, password },
-        },
-      });
-      navigate("/list");
-      window.location.reload();
-    } catch (error) {
-      console.error("Error during login:", error);
-      alert("Credenciais inválidas. Por favor, tente novamente.");
+    if (password === "") {
+      setPasswordError("O campo de senha não pode estar vazio.");
+      hasError = true;
+    } else if (!validatePassword.test(password)) {
+      setPasswordError(
+        "A senha deve ter no mínimo 7 caracteres sendo ao menos uma letra e um dígito",
+      );
+      hasError = true;
+    } else {
+      setPasswordError("");
+    }
+
+    if (name === "") {
+      setNameError("O campo de nome não pode estar vazio.");
+      hasError = true;
+    } else {
+      setNameError("");
+    }
+
+    if (phone === "") {
+      setPhoneError("O campo de telefone não pode estar vazio.");
+      hasError = true;
+    } else {
+      setPhoneError("");
+    }
+
+    if (birthDate === "") {
+      setBirthDateError("O campo da data de nascimento não pode estar vazio.");
+      hasError = true;
+    } else if (!validateBirthDate(birthDate)) {
+      setBirthDateError(
+        "A data de nascimento é inválida ou você é menor de 18 anos.",
+      );
+      hasError = true;
+    } else {
+      setBirthDateError("");
+    }
+
+    if (role === "") {
+      setRoleError("O campo da função não pode estar vazio.");
+      hasError = true;
+    } else {
+      setRoleError("");
+    }
+
+    if (!hasError) {
+      try {
+        await createUser({
+          variables: {
+            input: { name, email, phone, role, birthDate, password },
+          },
+        });
+        navigate("/list");
+        window.location.reload();
+      } catch (error) {
+        console.error("Error during login:", error);
+        alert("Credenciais inválidas. Por favor, tente novamente.");
+      }
     }
   }
   return (
-    <div className="Split-screen">
-      <div className="Left">
-        <img src={taq} alt="Taqtile" className="Logo" />
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="flex justify-center items-center h-20% md:h-full bg-[rgba(14,14,14,0.964)] w-full md:w-1/2">
+        <img
+          src={taq}
+          alt="Taqtile"
+          className="max-w-[40%] max-h-[40%] object-contain"
+        />
       </div>
-      <div className="Right">
+      <div className="flex justify-center items-center h-80% md:h-full w-full md:w-1/2">
         {loading ? (
           <Loading />
         ) : (
-          <form onSubmit={handleSignup} className="Form">
-            <h1 className="Header"> Cadastro </h1>
-            <div className="Form-group">
-              <label className="Label-text">Nome:</label>
+          <form onSubmit={handleSignup}>
+            <h1 className="text-center text-2xl font-bold text-black mt-5 mb-5">
+              Cadastro
+            </h1>
+            <div className="flex flex-col items-center">
+              <label
+                className={`text1-1 text-left w-52 font-regular mb-3 ${nameError ? "text-red-500" : "text-slate-700"}`}
+              >
+                Nome:
+              </label>
               <input
                 type="text"
-                className="Input-field"
+                className={`border rounded px-1 h-9 h-10 w-52 mb-3 ${nameError ? "border-red-500" : "border-slate-300"}`}
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
-              <label className="Label-text">E-mail:</label>
+              {nameError && (
+                <span className="text-red-500 text-xs w-52 regular  mb-1">
+                  {nameError}
+                </span>
+              )}
+
+              <label
+                className={`text1-1 text-left w-52 font-regular mb-3 ${emailError ? "text-red-500" : "text-slate-700"}`}
+              >
+                E-mail:
+              </label>
               <input
                 type="text"
-                className="Input-field"
+                className={`border rounded px-1 h-9 w-52 mb-3 ${emailError ? "border-red-500" : "border-slate-300"}`}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
+              {emailError && (
+                <span className="text-red-500 text-xs regular  mb-1 w-52">
+                  {emailError}
+                </span>
+              )}
 
-              <label className="Label-text">Senha:</label>
+              <label
+                className={`text1-1 text-left w-52 font-regular mb-3 ${passwordError ? "text-red-500" : "text-slate-700"}`}
+              >
+                Senha:
+              </label>
               <input
                 type="password"
-                className="Input-field"
+                className={`border rounded px-1 h-9 w-52 mb-3 ${passwordError ? "border-red-500" : "border-slate-300"}`}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+              {passwordError && (
+                <span className="text-red-500 text-xs regular  mb-1 w-52">
+                  {passwordError}
+                </span>
+              )}
 
-              <label className="Label-text">Telefone:</label>
+              <label
+                className={`text1-1 text-left w-52 font-regular mb-3 ${phoneError ? "text-red-500" : "text-slate-700"}`}
+              >
+                Telefone:
+              </label>
               <input
                 type="text"
-                className="Input-field"
+                className={`border rounded px-1 h-9 w-52 mb-3 ${phoneError ? "border-red-500" : "border-slate-300"}`}
                 value={phone}
                 onChange={(e) => setPhone(e.target.value)}
               />
+              {phoneError && (
+                <span className="text-red-500 text-xs regular  mb-1 w-52">
+                  {phoneError}
+                </span>
+              )}
 
-              <label className="Label-text">Data de nascimento:</label>
+              <label
+                className={`text1-1 text-left w-52 font-regular mb-3 ${birthDateError ? "text-red-500" : "text-slate-700"}`}
+              >
+                Data de nascimento:
+              </label>
               <input
                 type="text"
-                className="Input-field"
+                className={`border rounded px-1 h-9 w-52 mb-3 ${birthDateError ? "border-red-500" : "border-slate-300"}`}
                 value={birthDate}
                 onChange={(e) => setbirthDate(e.target.value)}
               />
+              {birthDateError && (
+                <span className="text-red-500 text-xs regular mb-1 w-52">
+                  {birthDateError}
+                </span>
+              )}
 
-              <label className="Label-text">Função:</label>
+              <label
+                className={`text1-1 text-left w-52 font-regular mb-3 ${roleError ? "text-red-500" : "text-slate-700"}`}
+              >
+                Função:
+              </label>
               <input
                 type="text"
-                className="Input-field"
+                className={`border rounded px-1 h-9 w-52 mb-3 ${roleError ? "border-red-500" : "border-slate-300"}`}
                 value={role}
                 onChange={(e) => setRole(e.target.value)}
               />
-
-              <button className="Button" type="submit">
+              {roleError && (
+                <span className="text-red-500 text-xs regular mb-1 w-52">
+                  {roleError}
+                </span>
+              )}
+              <button
+                className="bg-[#0b7b10] text-s text-white p-2 mt-5 w-44 h-12 border-none rounded-xl cursor-pointer transition ease duration-300 font-bold hover:bg-[#4adb64]"
+                type="submit"
+              >
                 Cadastrar
               </button>
             </div>
--- my-app/src/pages/Login.tsx
+++ my-app/src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import "../styles/Login.css";
 import { FormEvent, useState } from "react";
 import { validatePassword, validateEmail } from "../utils/Regex";
 import { useMutation } from "@apollo/client";
@@ -11,62 +10,102 @@ function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   async function handleSignIn(e: FormEvent) {
     e.preventDefault();
+    let hasError = false;
 
-    switch (true) {
-      case email === "":
-        return alert("O campo de email não pode estar vazio.");
-      case password === "":
-        return alert("O campo de senha não pode estar vazio.");
-      case !validateEmail.test(email):
-        return alert("Por favor digite um e-mail válido! ");
-      case !validatePassword.test(password):
-        return alert(
-          "A senha deve ter no mínimo 7 caracteres sendo ao menos uma letra e um dígito",
-        );
-      default:
+    if (email === "") {
+      setEmailError("O campo de email não pode estar vazio.");
+      hasError = true;
+    } else if (!validateEmail.test(email)) {
+      setEmailError("Por favor, insira um e-mail válido.");
+      hasError = true;
+    } else {
+      setEmailError("");
     }
 
-    try {
-      const result = await login({ variables: { input: { email, password } } });
-      const token = result.data.login.token;
-      localStorage.setItem("token", token);
-      navigate("/list");
-    } catch (error) {
-      console.error("Error during login:", error);
-      alert("Credenciais inválidas. Por favor, tente novamente.");
+    if (password === "") {
+      setPasswordError("O campo de senha não pode estar vazio.");
+      hasError = true;
+    } else if (!validatePassword.test(password)) {
+      setPasswordError(
+        "A senha deve ter no mínimo 7 caracteres sendo ao menos uma letra e um dígito",
+      );
+      hasError = true;
+    } else {
+      setPasswordError("");
+    }
+
+    if (!hasError) {
+      try {
+        const result = await login({
+          variables: { input: { email, password } },
+        });
+        const token = result.data.login.token;
+        localStorage.setItem("token", token);
+        navigate("/list");
+      } catch (error) {
+        console.error("Error during login:", error);
+        alert("Credenciais inválidas. Por favor, tente novamente.");
+      }
     }
   }
   return (
-    <div className="Login-container">
-      <h1 className="Login-h1"> Bem-vindo(a) à Taqtile!</h1>
+    <div className="flex flex-col h-screen items-center justify-center ">
+      <h1 className="text-2xl font-bold text-black mt-5 mb-5">
+        Bem-vindo(a) à Taqtile!
+      </h1>
       <form onSubmit={handleSignIn}>
-        <div className="Login">
-          <label className="Login-label-text">E-mail:</label>
+        <div className="flex flex-col">
+          <label
+            className={`text1-1 font-regular mb-3 ${emailError ? "text-red-500" : "text-slate-700"}`}
+          >
+            e-mail:
+          </label>
           <input
             type="text"
-            className="Login-input-field"
+            className={`border rounded px-1 h-9 ${emailError ? "border-red-500" : "border-slate-300"}`}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {emailError && (
+            <span className="text-red-500 text-xs regular mt-2 mb-2">
+              {emailError}
+            </span>
+          )}
         </div>
-        <div className="Login">
-          <label className="Login-label-text">Senha:</label>
+        <div className="flex flex-col">
+          <label
+            className={`text1-1 font-regular mb-3 ${passwordError ? "text-red-500" : "text-slate-700"}`}
+          >
+            senha:
+          </label>
           <input
             type="password"
-            className="Login-input-field"
+            className={`border rounded px-1 h-9 ${passwordError ? "border-red-500" : "border-slate-300"}`}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {passwordError && (
+            <span className="text-red-500 text-xs regular mt-2 mb-2">
+              {passwordError}
+            </span>
+          )}
         </div>
         {loading ? (
           <Loading />
         ) : (
-          <button className="Login-button" type="submit">
-            Entrar
-          </button>
+          <div className="flex flex-col items-center justify-center mt-5">
+            <button
+              className="text-base font-normal bg-green-500 hover:bg-green-700 text-white w-40 h-11 flex items-center justify-center rounded"
+              type="submit"
+            >
+              Entrar
+            </button>
+          </div>
         )}
       </form>
     </div>
